904efe55a9eb1546f7212092275de3ce8fbf8771
Micah Wood <micah@wpscholar.com>
2020-04-29 15:22:37 -0400

    Update file so that local test works properly.

--- local.js
+++ local.js
@@ -1,5 +1,7 @@
 var {handler} = require('./index');
 
-handler('https://bluehost.com')
-    .then(response => console.log(response))
-    .catch(error => console.error(error));
+handler({
+	url: 'https://www.bluehost.com/',
+})
+	.then(response => console.log(response))
+	.catch(error => console.error(error));
